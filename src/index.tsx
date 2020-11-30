96b6a320b0b1cfd2f689ec4e4d5402e5c86442e0
HoseungJang <hsjang.dev@gmail.com>
2020-11-30 23:02:38 +0900

    add player test

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactPlayer from "react-player";
 
-ReactDOM.render(<div>hello</div>, document.getElementById("root"));
+ReactDOM.render(
+  <ReactPlayer url={"https://youtu.be/V4p8He3vP40"} />,
+  document.getElementById("root")
+);
