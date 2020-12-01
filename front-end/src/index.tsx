35cf5d7402f7516ca2751f2ea3faaf1952313a5b
HoseungJang <hsjang.dev@gmail.com>
2020-12-01 23:54:10 +0900

    add react router test

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ReactPlayer from "react-player";
 
 ReactDOM.render(
-  <ReactPlayer url={"https://youtu.be/V4p8He3vP40"} />,
+  <BrowserRouter>
+    <Switch>
+      <Route
+        exact
+        path="/playlist"
+        component={() => <ReactPlayer url={"https://youtu.be/V4p8He3vP40"} />}
+      />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("main")
 );
