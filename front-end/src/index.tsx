ff14c40057b98fdbe6db768a70301812078ae38e
HoseungJang <hsjang.dev@gmail.com>
2020-12-02 22:59:43 +0900

    fix main

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,17 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import ReactPlayer from "react-player";
+import { Main } from "./routes";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route
-        exact
-        path="/playlist"
-        component={() => <ReactPlayer url={"https://youtu.be/V4p8He3vP40"} />}
-      />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("main")
-);
+ReactDOM.render(<Main />, document.getElementById("main"));
