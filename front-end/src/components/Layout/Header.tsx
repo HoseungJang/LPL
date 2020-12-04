af9688077a54aa8399b1084da7d7c9f4d2ee46b3
HoseungJang <hsjang.dev@gmail.com>
2020-12-04 22:56:09 +0900

    add Header

---
+++ front-end/src/components/Layout/Header.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Header: React.FC<{ toggleTheme: () => void }> = ({ toggleTheme }) => {
+  return (
+    <Container>
+      <div className="header-title">LPL</div>
+      <button className="toggle-theme-button" onClick={toggleTheme}></button>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: 70px;
+
+  display: flex;
+  align-items: center;
+
+  padding: 0 20px;
+
+  box-sizing: border-box;
+
+  background-color: ${({ theme }) => theme.header.background};
+
+  > * {
+    flex: 0;
+  }
+
+  > .header-title {
+    font-size: 2rem;
+    color: ${({ theme }) => theme.header.title};
+  }
+
+  > .toggle-theme-button {
+    width: 50px;
+    height: 20px;
+  }
+`;
