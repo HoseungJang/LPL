02420856934469bffd15ac54e8b9478337bc1719
HoseungJang <hsjang.dev@gmail.com>
2020-12-08 00:36:11 +0900

    fix width, height to global style

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,9 @@ import { PlaylistContextProvider } from "./contexts/Playlist";
 
 const GlobalStyle = createGlobalStyle`
   html, body {
+    width: 100%;
+    height: 100%;
+    
     margin: 0;
     padding: 0;
 
