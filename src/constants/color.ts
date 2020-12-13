7cab1f75b8ac53ec0464e4bd9dd3720b49076da7
HoseungJang <hsjang.dev@gmail.com>
2020-12-13 20:47:40 +0900

    add colors

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -1,4 +1,10 @@
 export enum Color {
   White = "#ffffff",
   Black = "#000000",
+  BlackTransparency50 = "rgba(0, 0, 0, 0.5)",
+  Grey = "#BDBDBD",
+  GreyTransparency50 = "rgba(189, 189, 189, 0.5)",
+  DarkGrey = "#848484",
+  LightGrey = "#D8D8D8",
+  LightBlack = "#424242",
 }
