883fb5a7ff514ab9c81dcbfaa82fdf664c31216d
HoseungJang <hsjang.dev@gmail.com>
2020-12-13 20:47:22 +0900

    main WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import styled from "styled-components";
+import { AddingVideoForm } from "./components/AddingVideoForm";
 
+import { Color } from "./constants/color";
+
+import { Background } from "./components/Background";
 import { Player } from "./components/Player";
+import { Playlist } from "./components/Playlist";
+
 import { usePlaylist } from "./contexts/Playlist";
-import { useThumbnail } from "./hooks/useThumbnail";
 
 export const App: React.FC = () => {
-  const { playlist, addToPlaylist } = usePlaylist();
-
-  const thumbnail = useThumbnail("V4p8He3vP40");
-
-  useEffect(() => {
-    if (thumbnail.status === "success") {
-      addToPlaylist({
-        title: "I'm the trend",
-        url: "https://youtu.be/V4p8He3vP40",
-        thumbnail: thumbnail.url,
-      });
-    }
-  }, [thumbnail.status]);
-
-  return playlist.length > 0 ? <Player video={playlist[0]} /> : null;
+  return (
+    <>
+      <Background imageURL={usePlaylist().playlist[0].thumbnail} />
+      <Container>
+        <div className="primary">
+          <Player video={usePlaylist().playlist[0]} />
+        </div>
+        <div className="secondary">
+          <AddingVideoForm />
+          <Playlist />
+        </div>
+      </Container>
+    </>
+  );
 };
+
+const Container = styled.div`
+  position: relative;
+
+  z-index: 1;
+
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+
+  padding: 10%;
+
+  box-sizing: border-box;
+
+  > .primary {
+    width: 50%;
+
+    padding-right: 3%;
+  }
+
+  > .secondary {
+    width: 50%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+
+    background-color: ${Color.BlackTransparency50};
+  }
+`;
