022a8e14c9d25968a17261148c81e0bb206d6a58
HoseungJang <hsjang.dev@gmail.com>
2020-12-19 15:48:21 +0900

    add video states

--- src/contexts/Playlist.tsx
+++ src/contexts/Playlist.tsx
@@ -12,6 +12,10 @@ type PlaylistContextValue = {
   playlist: Video[];
   addToPlaylist: (video: Omit<Video, "id">) => void;
   removeFromPlaylist: (id: string) => void;
+  currentVideo: Video | null;
+  playVideo: (video: Video | null) => void;
+  goToPrevVideo: () => void;
+  goToNextVideo: () => void;
 };
 
 const LOCAL_STORAGE_KEY = "LPL_playlist";
@@ -23,6 +27,8 @@ export const PlaylistContextProvider: React.FC = ({ children }) => {
     const json = localStorage.getItem(LOCAL_STORAGE_KEY);
     return json ? JSON.parse(json) : [];
   });
+
+  const [currentVideo, setCurrentVideo] = useState<Video | null>(null);
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(playlist));
@@ -37,8 +43,32 @@ export const PlaylistContextProvider: React.FC = ({ children }) => {
     setPlaylist((prev) => prev.filter((video) => video.id !== id));
   }, []);
 
+  const goToPrevVideo = useCallback(() => {
+    if (currentVideo) {
+      const currentVideoIndex = playlist.findIndex((v) => v.id === currentVideo.id);
+      setCurrentVideo(playlist[currentVideoIndex - 1] ?? playlist[playlist.length - 1]);
+    }
+  }, [playlist, currentVideo]);
+
+  const goToNextVideo = useCallback(() => {
+    if (currentVideo) {
+      const currentVideoIndex = playlist.findIndex((v) => v.id === currentVideo.id);
+      setCurrentVideo(playlist[currentVideoIndex + 1] ?? playlist[0]);
+    }
+  }, [playlist, currentVideo]);
+
   return (
-    <PlaylistContext.Provider value={{ playlist, addToPlaylist, removeFromPlaylist }}>
+    <PlaylistContext.Provider
+      value={{
+        playlist,
+        addToPlaylist,
+        removeFromPlaylist,
+        currentVideo,
+        playVideo: setCurrentVideo,
+        goToPrevVideo,
+        goToNextVideo,
+      }}
+    >
       {children}
     </PlaylistContext.Provider>
   );
